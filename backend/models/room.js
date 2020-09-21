e4cf95826d64b4119fcc658810ef3df5532385e7
tim <tevtushok@ukr.net>
2020-09-21 19:40:32 +0300

    added room model

---
+++ backend/models/room.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const roomSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        min:3,
+        max: 255,
+        required: [true, 'Room title is required'],
+		unique: true
+    },
+    number: {
+        type: Number,
+        required: [true, 'Room number is required'],
+		unique: true,
+    },
+    status: {
+        type: Number,
+        required: true,
+        // 1 available
+        // 0 not available
+        default: 1
+    }
+})
+
+module.exports = mongoose.model('Room', roomSchema)
