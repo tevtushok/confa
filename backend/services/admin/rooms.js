be9d86a1b06ed8f4fb7206a8b439b7f3bb83a350
tim <tevtushok@ukr.net>
2020-09-25 14:29:38 +0300

    regular

--- backend/services/admin/rooms.js
+++ backend/services/admin/rooms.js
@@ -18,7 +18,7 @@ const listRooms = async (req, res) => {
 const saveRooms = async (req, res) => {
     const reqRoom = req.body.rooms;
     // single item to save
-    if (reqRoom instanceof Object) {
+    if (reqRoom instanceof Object && Object.keys(reqRoom).length) {
         const roomId = reqRoom._id;
         let room = reqRoom;
         if (roomId)
@@ -39,20 +39,45 @@ const saveRooms = async (req, res) => {
         }
         // create
         else {
-            await Room.create(room)
-            // return handleResponse(req, res, 200, API_CODES.SUCCESS, savedRoom, 'Saved');
+            let fields = {};
+
+            if (!reqRoom.title || reqRoom.title.length < 3) {
+                fields.title = reqRoom.title;
+            }
+            console.log(typeof reqRoom.number)
+            if (!reqRoom.number || isNaN(reqRoom.number)) {
+                fields.number = reqRoom.number;
+            }
+            if (Object.keys(fields).length) {
+                return handleResponse(req, res, 400, API_CODES.ERROR_ADMIN_SAVING_ROOMS_INPUT, {fields: fields}, 'Error while updating single room item in database');
+            }
+            console.log('----------------------------')
+            console.log(fields);
+            console.log(reqRoom.title.length);
+            console.log('----------------------------')
+            await Room.create(room, (err, savedRoom) => {
+                if (err) {
+                    // codeName: 'DuplicateKey',
+                    if (11000 === err.code) {
+                        const ret = {fields: err.keyValue}
+                        return handleResponse(req, res, 400, API_CODES.ERROR_ADMIN_SAVING_ROOMS_DUPLICATE, ret, 'Duplicate field');
+                    }
+                    return handleResponse(req, res, 400, API_CODES.ERROR_ADMIN_SAVING_ROOMS_FAILURE, err, 'Error while saving single room item in database');
+                }
+                return handleResponse(req, res, 200, API_CODES.SUCCESS, {room: savedRoom}, 'Saved');
+            })
         }
             
     }
     // invalid input params
     else {
-        return handleResponse(req, res, 500, API_CODES.ERROR_ADMIN_SAVING_ROOMS_FAILURE, null, 'Invalid input parameters');
+        return handleResponse(req, res, 500, API_CODES.ERROR_ADMIN_SAVING_ROOMS_INPUT, null, 'Invalid input parameters');
     }
 }
 
 const deleteRoom = async (req, res) => {
     try {
-        Room.findOneAndDelete({id: req.id }, function (err, docs) {
+        Room.findOneAndDelete({id: req.id }, function (err, room) {
             if (err){
                 return handleResponse(req, res, 500, API_CODES.FAILURE, err, 'Error while deleting room');
             }
