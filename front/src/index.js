76889bad59de7e5c0eb1bc93573af42e69490721
tim <tevtushok@ukr.net>
2021-04-25 11:22:53 +0300

    use customize-cra + react-app-rewired instead configuration within npm eject. I love decorators. Also move to react 17.0.2

--- front/src/index.js
+++ front/src/index.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
-
 import { Provider } from 'mobx-react';
 import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
 import { Router } from 'react-router';
-
+import reportWebVitals from './reportWebVitals';
 import userStore from './stores/userStore';
 import appStore from './stores/appStore';
-
-import './index.scss';
-import './App.scss'
-
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
 
 
 const browserHistory = createBrowserHistory();
 const routingStore = new RouterStore();
-
 const stores = { userStore, appStore, };
-
 const history = syncHistoryWithStore(browserHistory, routingStore);
 
 // <React.StrictMode>
@@ -29,14 +23,16 @@ const history = syncHistoryWithStore(browserHistory, routingStore);
 ReactDOM.render(
   <Provider {...stores}>
     <Router history={history}>
-      <App />
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
     </Router>
   </Provider>,
   document.getElementById('root')
 );
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
