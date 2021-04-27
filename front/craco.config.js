8c418b02a787b0b15dc38982c59a153711bded56
tim <tevtushok@ukr.net>
2021-04-27 18:10:47 +0300

    added measure module for loading webpack

--- front/craco.config.js
+++ front/craco.config.js
@@ -1,7 +1,22 @@
+const path = require('path');
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+
+const smp = new SpeedMeasurePlugin({
+    outputFormat: "humanVerbose",
+});
+
 module.exports = {
     babel: {
         plugins: [
             ["@babel/plugin-proposal-decorators", { "legacy": true }],
         ]
-    }
+    },
+    eslint: {
+        enable: false,
+    },
+    webpack: {
+        plugins: {
+            add: [smp],
+        },
+    },
 };
