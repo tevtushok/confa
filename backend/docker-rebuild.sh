8e831de41c0071819b7af380fed80c5200fdabd7
tim <tevtushok@ukr.net>
2021-04-28 15:56:38 +0300

    colorize

--- backend/docker-rebuild.sh
+++ backend/docker-rebuild.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 imageName=confa-img
 containerName=confa-api
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 
 GREEN=`tput setaf 2`
 YELLOW=`tput setaf 3`
-RESET=`tput sgr0`
+NC='\033[0m' # No Color
 
 docker build -t $imageName -f Dockerfile  .
 
@@ -14,9 +12,9 @@ psRes=$(docker ps -aq -f name="$containerName")
 
 if [[ ! -z "$psRes" ]];
 then
-    printf "${YELLOW}delete old container${RESET} $containerName"
+    printf "${YELLOW}delete old container${NC} $containerName"
     docker rm -fv $containerName
 fi
 
-printf "${GREEN}Run new container... ${RESET} $containerName "
+printf "${GREEN}Run new container... ${NC} $containerName "
 docker run -d -p 49160:4000 --name $containerName $imageName
